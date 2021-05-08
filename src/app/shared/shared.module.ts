9141bc012d89e31b10606113fb38000521bcb2fa
Neha Shetty <nehashettyn06@gmail.com>
2021-05-08 18:04:20 +0530

    initial setup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HeaderModule } from './header/header.module';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [CommonModule, HeaderModule],
+  exports: [HeaderModule],
+})
+export class SharedModule {}
